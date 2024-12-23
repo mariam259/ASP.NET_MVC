2d976c919a15bc56fb31e3dc5082edb207024aff
mariam259 <mariamahmed6441@gamil.com>
2024-12-23 15:36:23 +0200

    add a PK

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,13 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
 
 using System.ComponentModel.DataAnnotations;
 namespace mvc.Models
 {
     public class AppUser 
     {
+        [Key]
+        public string Id { get; set; }
         public Address? Address { get; set; }
         public int? Pace { get; set; }
 
