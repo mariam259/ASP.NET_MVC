f05495958b84eeeba13f15d4557ccb5bd1dce49a
mariam259 <mariamahmed6441@gamil.com>
2024-12-26 23:26:59 +0200

    add repository

---
+++ Repository/ClubRepository.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using mvc.Data;
+using mvc.Interfaces;
+using mvc.Models;
+
+namespace mvc.Repository
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ClubRepository(ApplicationDBContext context)
+        {
+            _context = context;
+
+        }
+
+        public bool Add(Club club)
+        {
+            _context.Add(club); //this Add function generate the sql query for adding a club to DB
+            return Save();  //when we implement the save function it the moment we truly run the query and save the club to the DB
+        }
+
+        public bool Delete(Club club)
+        {
+            _context.Remove(club);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Club>> GetAlls()
+        {
+            return await _context.Clubs.ToListAsync();
+        }
+
+        public async Task<Club> GetByIdAsync(int id)
+        {
+            return await _context.Clubs.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<IEnumerable<Club>> GetClubByCity(string city)
+        {
+            return await _context.Clubs.Where(c => c.Address.City.Contains(city)).ToListAsync();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved >= 0 ? true : false;
+        }
+
+        public bool Update(Club club)
+        {
+            _context.Update(club);
+            return Save();
+        }
+    }
+}
---
+++ Repository/RaceRepository.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using mvc.Data;
+using mvc.Interfaces;
+using mvc.Models;
+
+namespace mvc.Repository
+{
+    public class RaceRepository : IRaceRepository
+
+    {
+        private readonly ApplicationDBContext _context;
+        public RaceRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Race race)
+        {
+            _context.Add(race);
+            return Save();
+        }
+
+        public bool Delete(Race race)
+        {
+            _context.Remove(race);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Race>> GetAll()
+        {
+            return await _context.Races.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Race>> GetAllRacesByCity(string city)
+        {
+            return await _context.Races.Where(r => r.Address.City.Contains(city)).ToListAsync();
+        }
+
+        public async Task<Race> GetByIdAsync(int id)
+        {
+            return await _context.Races.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public bool Save()
+        {
+            var save = _context.SaveChanges();
+            return save > 0 ? true : false;
+        }
+
+        public bool Update(Race race)
+        {
+            _context.Update(race);
+            return Save();
+        }
+    }
+}
