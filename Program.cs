f076c14b1e62c48585289488f1800a02ad8417ce
mariam259 <mariamahmed6441@gamil.com>
2024-12-26 23:31:38 +0200

    add repos

--- Program.cs
+++ Program.cs
@@ -1,9 +1,42 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using mvc.Data;
+using mvc.Interfaces;
+using mvc.Repository;
+// using mvc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IClubRepository, ClubRepository>();
+builder.Services.AddScoped<IRaceRepository, RaceRepository>();
 
+//connect to database using services
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
+
+var connectionString = "data source=DESKTOP-NCPJBUG\\SQLEXPRESS;initial catalog=RunGroups;user id=mariam;password=sql2024;TrustServerCertificate=True";
+using (var connection = new SqlConnection(connectionString))
+{
+    try
+    {
+        connection.Open();
+        Console.WriteLine("Connection successful!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Connection failed: {ex.Message}");
+    }
+}
+
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+{
+    Seed.SeedData(app);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
